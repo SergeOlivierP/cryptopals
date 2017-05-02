d98df80320c67374bfc48c2325677fd5b2919067
soruso <sergeolivierp@gmail.com>
2017-05-01 21:36:35 -0400

    ch7 added

---
+++ ch7.py
@@ -0,0 +1,12 @@
+import base64
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+backend = default_backend()
+key = b'YELLOW SUBMARINE'
+x = base64.b64decode(open('7.txt', 'r').read())
+
+cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
+decryptor = cipher.decryptor()
+dec = decryptor.update(x) + decryptor.finalize()
+print(dec.decode("utf-8"))
