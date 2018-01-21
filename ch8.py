6fec972034bef74ccbd25b7c501048fdfd1f63f3
serge <serge-olivier.paquette@delvelabs.ca>
2018-01-21 14:05:19 -0500

    fix

--- ch8.py
+++ ch8.py
@@ -1,35 +1,27 @@
-from operator import itemgetter
 import binascii
 
 
-def decodeLines(filename):
-
-    with open(filename, 'r') as f:
-        c = f.read()
-
-    for line in c:
+def decodeLines(lines):
+    s = ""
+    for line in lines:
         if line[-1] == '\n':
             line = line[:-1]
         s = binascii.unhexlify(line)
         yield s
 
 
-def splitAES(cipher):
-    k = 16
-    return [cipher[i:i+k] for i in range(0, len(cipher), k)]
-
-
 def isECB(line):
-    blocks = splitAES(line)
+    k = 16
+    blocks = [line[i:i+k] for i in range(0, len(line), k)]
     return any(blocks.count(x) > 1 for x in blocks)
 
 
 if __name__ == "__main__":
 
-    cipherTxt = decodeLines('cipherfile4')
-    index = 0
-    for line in cipherTxt:
-        index += 1
+    with open('cipherfile4', 'r') as f:
+        c = f.readlines()
+
+    cipherTxt = decodeLines(c)
+    for i, line in enumerate(cipherTxt):
         if isECB(line):
-            print(line)
-            print('Line:', index)
+            print("line: {} : {}".format(i, line))
